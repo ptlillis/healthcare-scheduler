7911859e329144577569ae0653fb882c72ca69ef
Mary C <mcn4c@virginia.edu>
2021-02-25 18:26:37 -0500

    added appointment api route

--- public/js/login.js
+++ public/js/login.js
@@ -11,16 +11,16 @@ $(document).ready(() => {
         email: emailInput.val().trim(),
         password: passwordInput.val().trim()
       };
-  
+      
       if (!userData.email || !userData.password) {
-        return;
-      }
-  
-      // If we have an email and password we run the loginUser function and clear the form
-      loginUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
-    });
+      return;
+    }
+    
+    // If we have an email and password we run the loginUser function and clear the form
+    loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
   
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
     function loginUser(email, password) {

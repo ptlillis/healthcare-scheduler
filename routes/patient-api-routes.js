be4a220b3db3740965b18f43adfba00be5530175
Daniel Mestawat <dannymamusha@gmail.com>
2021-03-05 16:11:58 -0500

    update
    update

--- routes/patient-api-routes.js
+++ routes/patient-api-routes.js
@@ -1,101 +1,43 @@
-// Dependencies
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
-// Requiring our models
-const db = require('../models');
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
-// Routes
-module.exports = (app) => {
-  app.get('/api/patient', (req, res) => {
-    const query = {};
-    if (req.query.patient_id) {
-      query.UserId = req.query.patient_id;
-    }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.User
-    db.patient.findAll({
-      where: query,
-      include: [db.Provider],
-    }).then((dbPatient) => res.json(dbPatient));
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+require("./routes/patient-api-routes.js")(app);
+require("./routes/provider-api-routes.js")(app);
+// require("./routes/appointment-api-routes.js")(app);
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
+});
 
-  // Get route for retrieving a single patient
-  app.get('/api/patient/:id', (req, res) => {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.User
-    db.patient.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [db.Provider],
-    }).then((dbPatient) => res.json(dbPatient));
-  });
-
-  // POST route for saving a new patient
-  app.post('/api/patient', (req, res) => {
-    console.log('======================',req.body);
-    db.Patient.create(req.body).then((dbPatient) => {
-    console.log(dbPatient.dataValues.patient_id);
-    //get providers 
-    db.Provider.findAll({
-      where: {
-        monday: dbPatient.dataValues.monday,
-        tuesday: dbPatient.dataValues.tuesday,
-        wednesday: dbPatient.dataValues.wednesday,
-        thursday: dbPatient.dataValues.thursday,
-        friday: dbPatient.dataValues.friday,
-        saturday: dbPatient.dataValues.saturday,
-        sunday: dbPatient.dataValues.sunday,
-      }
-    }
-    ).then((dbProvider) => {
-    console.log(dbProvider[0].dataValues.provider_id,
-      dbProvider[0].dataValues.provider_First_name,
-      dbProvider[0].dataValues.provider_last_name,
-      dbProvider[0].dataValues.license)
-     res.json(dbPatient)
-    const newAppointment = 
-    {
-      patient_id: dbPatient.dataValues.patient_id,
-      patient_First_name: dbPatient.dataValues.patient_First_name,
-      patient_Last_name: dbPatient.dataValues.patient_Last_name,
-      patient_Address: dbPatient.dataValues.patient_Address,
-      medical_needs: dbPatient.dataValues.medical_needs,
-      insurance_Type: dbPatient.dataValues.insurance_Type,
-      provider_id: dbProvider[0].dataValues.provider_id,
-      provider_First_name: dbProvider[0].dataValues.provider_First_name,
-      provider_last_name: dbProvider[0].dataValues.provider_last_name,
-      email: dbProvider[0].dataValues.email,
-      license: dbProvider[0].dataValues.license,
-      monday: dbPatient.dataValues.monday,
-      tuesday: dbPatient.dataValues.tuesday,
-      wednesday: dbPatient.dataValues.wednesday,
-      thursday: dbPatient.dataValues.thursday,
-      friday: dbPatient.dataValues.friday,
-      saturday: dbPatient.dataValues.saturday,
-      sunday: dbPatient.dataValues.sunday,
-     
-
-    }
-    //return
-    db.Appointment.create(newAppointment).then((dbAppointment) => {
-      console.log(dbAppointment.dataValues);
-    })})
-  })})
-    app.delete('/api/patient/:id', (req, res) => {
-      db.Patient.destroy({
-        where: {
-          id: req.params.id,
-        },
-      }).then((dbPatient) => res.json(dbPatient));
-    });
-    // PUT route for updating patients
-    app.put('/api/patient', (req, res) => {
-      db.Patient.update(req.body, {
-        where: {
-          id: req.body.id,
-        },
-      }).then((dbPatient) => res.json(dbPatient));
-    });
-  };
